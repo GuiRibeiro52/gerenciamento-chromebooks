3d786f540269ec78d17c1d2974efb25cec8afd9f
Guilherme Ribeiro <guilherme_ribeiro52@icloud.com>
2024-12-03 19:11:44 -0300

    iniciando projeto chromebook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header"; // Importando o header fixo
+import Login from "./pages/Login"; // Página de Login
+import Home from "./pages/Home"; // Página inicial
+import Professores from "./pages/Professores"; // Listagem de professores
+import ProfessorForm from "./pages/ProfessorForm"; // Formulário de cadastro/edição de professores
+import Chromebooks from "./pages/Chromebooks"; // Listagem de Chromebooks
+import ChromebookForm from "./pages/ChromebookForm"; // Formulário de cadastro/edição de Chromebooks
+import EmprestarChromebook from "./pages/EmprestarChromebook"; // Página de empréstimo de Chromebook
+import DevolverChromebook from "./pages/DevolverChromebook"; // Página de devolução de Chromebook
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header /> {/* Incluindo o Header fixo */}
+      <div className="main-content">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/professores" element={<Professores />} />
+          <Route path="/professores/novo" element={<ProfessorForm />} />
+          <Route path="/professores/editar/:id" element={<ProfessorForm />} />
+          <Route path="/chromebooks" element={<Chromebooks />} />
+          <Route path="/chromebooks/novo" element={<ChromebookForm />} />
+          <Route path="/chromebooks/editar/:id" element={<ChromebookForm />} />
+          <Route path="/emprestar" element={<EmprestarChromebook />} />
+          <Route path="/devolver" element={<DevolverChromebook />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
